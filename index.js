24090b16a2d11db82ae42b5cc5b164b959a1e2b3
Vinay <vinay.kumar@nybinfotech.com>
2026-05-25 16:30:26 +0530

    chsnges made

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+let fullName = "vinay";
+let age = "22";
+console.log(fullName,age);
+// console.log(age);
+}
+{
+const pi = 2.31;
+console.log("pi");
+}
+{
+let name = "yashwanth";
+let age = "22";
+console.log(name);
+console.log(age)
+}
