f8cf3f04786a3e7b8cdb7c2e3a7c61bb9d003245
Vinay <vinay.kumar@nybinfotech.com>
2026-05-26 15:46:58 +0530

    changes made

--- index.js
+++ index.js
@@ -9,8 +9,49 @@ const pi = 2.31;
 console.log("pi");
 }
 {
-let name = "yashwanth";
-let age = "22";
+var name = "yashwanth";
+var age = "22";
 console.log(name);
 console.log(age)
 }
+
+ let fullname = prompt("enter your name without spaces");  
+
+ let username = "@"+ fullname + fullname.length;
+ console.log(username);
+
+ let n= alert("hello and welcome!");
+
+
+ 
+// 1. Number
+let age1 = 25;
+
+// 2. String
+let name1 = "Vinay";
+
+// 3. Boolean
+let isLoggedIn = true;
+let hascompleted = false;
+
+// 4. Undefined
+let x;
+console.log("Undefined value of x:", x);
+
+// 5. Null
+let Value = null;
+
+
+// 6. BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+
+// 7. Symbol
+let uniqueId = Symbol("id");
+
+
+console.log("Number:", age1);
+console.log("String:", name1);
+console.log("Boolean:", isLoggedIn, hascompleted);
+console.log("Null:", Value);
+console.log("BigInt:", bigNumber);
+console.log("Symbol:", uniqueId);
