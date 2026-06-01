7a0b47da44513df72cdcfc552f83e1f37a7de0d2
Vinay <vinay.kumar@nybinfotech.com>
2026-06-01 16:15:30 +0530

    changes made

--- JavaScript_01_06_2026/funtions.js
+++ JavaScript_01_06_2026/funtions.js
@@ -6,3 +6,51 @@ function greetuser(vinay){
 }
 
 greetuser("vinay");
+
+//function without parameters
+
+function greet(){
+    console.log("hello and welcome to nyb!");
+
+}   
+greet();
+
+//functions with using default parameters
+
+function greetuser1(name = "guest") {
+    console.log("hello" + name);
+
+}
+greetuser1();
+greetuser("vinay");
+
+//return-a function can return a value using the return statement.
+
+function multiply (a,b=6) {
+    return a *b;
+
+}
+console.log(multiply(5));
+console.log(multiply(6,4));
+
+
+//using argument objecct.
+
+function showArguments() {
+    console.log(arguments);
+    console.log("first argument :",arguments[0]);
+    console.log("second argument :",arguments[1]);
+}
+showArguments("vinay",22,);
+
+//rest parameters--collect multiple arguements into an array
+
+function sum1 (...numbers) {
+    let total = 0;
+    for (let num of numbers) {
+        total +=num;
+    }
+    return total;
+}
+
+console.log(sum1(10,20,37,63));
