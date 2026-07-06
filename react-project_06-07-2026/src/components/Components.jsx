68ae0806fba5dff6024b803199d242394b7d0c53
Vinay <vinay.kumar@nybinfotech.com>
2026-07-06 19:15:23 +0530

    changes made

---
+++ react-project_06-07-2026/src/components/Components.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Components() {
+  return (
+    <div>
+      <h1>MY DETAILS</h1>
+      <p>hello guys im vinay</p>
+
+    </div>
+  );
+}
+export default Components;
